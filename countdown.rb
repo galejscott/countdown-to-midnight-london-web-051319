ec0dc7e8bdf4f567b0cb2d6bc07f16a574840753
Scott Gale <galejscott@gmail.com>
2019-05-05 13:35:10 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,21 +2,17 @@
 
 def countdown(n)
   while n > 0
-    puts "#{n} SECONDS!"
+    puts "#{n} SECOND(S)!"
     n -= 1
   end
   return "HAPPY NEW YEAR!"
 end
 
-countdown(10)
-
 def countdown_with_sleep(n)
   while n > 0 
     sleep 1
-    puts "#{n} SECONDS!"
+    puts "#{n} SECOND(S)!"
     n -= 1 
   end
   puts "HAPPY NEW YEAR!"
 end
-
-countdown_with_sleep(10)
